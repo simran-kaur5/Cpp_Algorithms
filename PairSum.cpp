db04ffee4b75cbd52acf9b35eac94ae670af5d15
Hrudhay.H <145639412+Hrudhay-H@users.noreply.github.com>
2024-11-10 14:12:07 +0530

    Update PairSum.cpp

--- PairSum.cpp
+++ PairSum.cpp
@@ -1,32 +1,41 @@
-//This pairsum approach worls only for sorted numbers
-
 #include<iostream>
 #include<vector>
 using namespace std;
-vector<int>pairSum(vector<int> nums,int target) {
-    int n=nums.size();
-    vector<int>ans;
-    int i=0,j=n-1;
-    while(i<j) {
-        int pairsum=nums[i]+nums[j];
-        if(pairsum>target) {
+
+// This function finds two indices in a sorted vector whose values sum up to the target value.
+vector<int> pairSum(vector<int> nums, int target) {
+    int n = nums.size();
+    vector<int> ans;
+    int i = 0, j = n - 1; // Set pointers at the beginning and end of the vector
+
+    // Loop until the pointers cross each other
+    while (i < j) {
+        int pairsum = nums[i] + nums[j]; // Calculate the sum of elements at pointers
+
+        // If pairsum is greater than target, move the end pointer to the left
+        if (pairsum > target) {
             j--;
         }
-        else if(pairsum<target) {
+        // If pairsum is less than target, move the start pointer to the right
+        else if (pairsum < target) {
             i++;
         }
+        // If pairsum equals target, store the indices and return the result
         else {
             ans.push_back(i);
             ans.push_back(j);
             return ans;
         }
     }
-return ans;
+    return ans; // Return empty if no such pair exists
 }
+
 int main() {
-    vector<int>nums={2,7,18,39};
-    int target=46;
-    vector<int>ans=pairSum(nums,target);
-    cout<<ans[0]<<", "<<ans[1]<<endl;
+    vector<int> nums = {2, 7, 18, 39}; // Sorted array
+    int target = 46;
+    vector<int> ans = pairSum(nums, target);
+
+    // Output the result
+    cout << ans[0] << ", " << ans[1] << endl;
     return 0;
 }
