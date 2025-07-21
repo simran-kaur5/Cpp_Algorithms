762d7cffd6664b8e72d1c43b885d999dda27363d
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-07-21 17:56:37 +0530

    Update Solution class for leetcode

--- Leetcode_1.cpp
+++ Leetcode_1.cpp
@@ -1,57 +1,45 @@
 // Copy only the CLASS block to sumbit code on Leetcode !!
 
 #include <iostream>
-#include<vector>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-// Define a class named Solution
-class Solution
-{
-    public:
-    // Declare variables
-    int arr[10]; // Array to hold a maximum of 10 elements
-    int target, n, i, j; // Variables for target, size of array, and iterators
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m; // Map number -> index
+        vector<int> ans;              // Vector to store the answer indices
 
-    public:
-    // Function to find two indices in the array whose values sum up to the target
-    int twoSum()
-    {
-        // Prompt user to enter the size of the array
-        cout << "Enter the size of array < 10\n";
-        cin >> n; // Read the size of the array
+        for (int i = 0; i < nums.size(); i++) {
 
-        // Prompt user to enter the elements of the array
-        cout << "Enter the array elements\n";
-        for (i = 0; i < n; i++) { // Loop to read 'n' elements
-            cin >> arr[i];
-        }
-
-        // Prompt user to enter the target sum
-        cout << "Enter the target element\n";
-        cin >> target;
-
-        // Nested loop to find two indices whose elements add up to the target
-        for (i = 0; i < n; i++) { // Outer loop
-            for (j = 0; j < n; j++) { // Inner loop
-                // Check if the sum of elements at indices i and j equals the target
-                if (arr[i] + arr[j] == target) {
-                    // Print the indices and values of the two elements
-                    cout << "The indexes are " << i << " and " << j << endl;
-                    cout << "The value at those indexes are " << arr[i] << " and " << arr[j] << endl;
-                    cout << arr[i] << " + " << arr[j] << " = " << target;
-
-                    return 0; // Exit the function once a pair is found
-                }
+            if (m.find(target - nums[i]) != m.end()) {
+                ans.push_back(m[target - nums[i]]); // index of target - nums
+                ans.push_back(i);                 // current index
+                return ans;                      // return the vector
             }
-        }
 
-        return 0; // Return 0 if no pair is found
+            m[nums[i]] = i;
+        }
+        
+        return ans; // will be empty if no solution found
     }
 };
 
-// Main function to execute the program
 int main() {
-    Solution a; // Create an object of the Solution class
-    a.twoSum(); // Call the twoSum function
-    return 0; // Return 0 to indicate successful execution
+    Solution sol;
+    //You test more cases by changing inputs
+    // Simple test case 
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No two sum solution found." << endl;
+    }
+
+    return 0;
 }
